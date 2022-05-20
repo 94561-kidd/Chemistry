effe2e306b389b6af4060732f7e25cfe9a451ccd
94561-kidd <94561aaaa@gmail.com>
2022-05-20 14:36:45 +0800

    update

--- index/js_1.js
+++ index/js_1.js
@@ -1,3 +1,13 @@
+var getUrlString = location.href;
+var url = new URL(getUrlString);
+document.getElementById("doc").src = "./" + url.searchParams.get("doc") + ".html";
+
 function changeSrc(target) {
+  var new_url =
+    "https://94561-kidd.github.io/Chemistry/index.html?doc=" + target;
+
+
+  location.href = new_url;
+  
   document.getElementById("doc").src = "./" + target + ".html";
 }
