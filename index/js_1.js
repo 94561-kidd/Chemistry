da063a5e34f6704b95f6a946291db378aacbbe80
94561-kidd <94561aaaa@gmail.com>
2022-05-20 23:41:25 +0800

    Update js_1.js

--- index/js_1.js
+++ index/js_1.js
@@ -1,7 +1,7 @@
-const urlParams = new URLSearchParams(window.location.search);
-var docstr = urlParams.get("docname");
+var urlParams = new URLSearchParams(window.location.search);
 
 if (urlParams.has("docname")) {
+  var docstr = urlParams.get("docname");
   document.getElementById("doc").src = `./${docstr}.html`;
 
   var selected_menu_button = document.getElementById(docstr);
@@ -13,23 +13,25 @@ if (urlParams.has("docname")) {
 }
 
 function changeSrc(target) {
+  var url_params = new URLSearchParams(window.location.search);
+
+  if (url_params.has("docname")) {
+    var docstr = url_params.get("docname");
+
+    var cancel_selected_menu_button = document.getElementById(docstr);
+    cancel_selected_menu_button.style.background = "rgba(0, 0, 0, 0)";
+    cancel_selected_menu_button.style.color = "#fff";
+    cancel_selected_menu_button.disabled = false;
+  }
+
   var new_url = `/Chemistry/index.html?docname=${target}`;
   window.history.pushState(null, "", new_url);
   document.getElementById("doc").src = "./" + target + ".html";
 
-  var cancel_selected_menu_button = document.getElementById(docstr);
-  cancel_selected_menu_button.style.background = "rgba(0, 0, 0, 0)";
-  cancel_selected_menu_button.style.color = "#fff";
-  cancel_selected_menu_button.disabled = false;
-
-  docstr = target;
-
-  var selected_menu_button = document.getElementById(docstr);
+  var selected_menu_button = document.getElementById(target);
   selected_menu_button.style.background = "#f3f2ee";
   selected_menu_button.style.color = "#333333";
   selected_menu_button.disabled = true;
 
-  document.title = "化學 • " + document.getElementById(docstr).innerHTML;
-
-  selected_style();
+  document.title = "化學 • " + document.getElementById(target).innerHTML;
 }
