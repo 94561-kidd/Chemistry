47e8eafaa95886fa989d9d94982926e54fd7868d
94561-kidd <94561aaaa@gmail.com>
2022-05-20 23:21:23 +0800

    Update js_1.js

--- index/js_1.js
+++ index/js_1.js
@@ -12,16 +12,17 @@ function changeSrc(target) {
   var new_url = `/Chemistry/index.html?docname=${target}`;
   window.history.pushState(null, "", new_url);
   document.getElementById("doc").src = "./" + target + ".html";
+
+  var menu_button = document.querySelector("button.menu-option");
+  menu_button.style.background = "rgba(0, 0, 0, 0)";
+  menu_button.style.color = "#fff";
+  menu_button.disabled = false;
+
   selected_style();
 }
 
 function selected_style() {
   document.title = "化學 • " + document.getElementById(docstr).innerHTML;
-
-  var menu_button = document.querySelector('button.menu-option');
-  menu_button.style.background = "rgba(0, 0, 0, 0)";
-  menu_button.style.color = "#fff";
-  menu_button.disabled = false;
 
   var selected_menu_button = document.getElementById(docstr);
   selected_menu_button.style.background = "#f3f2ee";
